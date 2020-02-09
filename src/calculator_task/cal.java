41c51f9c7fdf84fec053a1f030632db355739d7d
keerthipriyakutumbaka <keerthikutumbka@gmail.com>
2020-02-09 14:53:14 +0530

    my commit

---
+++ src/calculator_task/cal.java
@@ -0,0 +1,24 @@
+package calculator_task;
+
+public class cal {
+	
+		 public float sum(float a,float b)
+		 {
+			 return a+b;
+		 }
+		 public float diff(float a,float b)
+		 { 
+	         return a-b;
+		 }
+		public float div(float a,float b)
+		 {
+			 return a/b;
+		 }
+		 
+		 public float mul(float a,float b)
+		 {
+			 return a*b;
+		 }
+		
+	}
+
